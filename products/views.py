923f251dbd5c3d4f63a4a4e2b88cda0d4dc7b897
mateuszniechwiej <niechwiej.mateusz@gmail.com>
2021-10-05 10:47:32 +0100

    Add products templatate,urls,views and test

--- products/views.py
+++ products/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+def display_products(request):
+    """
+    A view to test displaying products
+    """
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
+    
