48475c393cfb1f8e7a3dcc761b1f3877c7d656f2
mateuszniechwiej <niechwiej.mateusz@gmail.com>
2021-10-09 15:16:11 +0100

    Add Variation model to products models.py

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Category
+from .models import Product, Category, Variation
 
 # Register your models here.
 
@@ -26,3 +26,4 @@ class CategoryAdmin(admin.ModelAdmin):
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Variation)
--- products/models.py
+++ products/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from djmoney.models.fields import MoneyField
 
+SIZE_CHOICES = (
+    ('small', 'S'),
+    ('medium', 'M'),
+    ('large', 'L'),
+)
+
 class Category(models.Model):
     """
     Category model
@@ -17,6 +23,7 @@ class Category(models.Model):
     def get_friendly_name(self):
         return self.friendly_name
 
+
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
@@ -24,10 +31,24 @@ class Product(models.Model):
     has_sizes = models.BooleanField(default=False, null=True, blank=True)
     has_diff_colors = models.BooleanField(default=False, null=True, blank=True)
     description = models.TextField()
-    price = MoneyField(max_digits=10, decimal_places=2, null=True, default_currency='EUR')
+    price = MoneyField(max_digits=10, decimal_places=2, null=True, blank=True, default_currency='EUR')
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
+
+
+class Variation(models.Model):
+    """
+    Model for product price per size variations
+    """
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    size = models.CharField(max_length=120, choices=SIZE_CHOICES, default='small')
+    price = MoneyField(max_digits=10, decimal_places=2, null=True, blank=True, default_currency='EUR')
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.size
+
