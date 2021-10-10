e8c28283852f7513441aba490c02e4d968973b63
mateuszniechwiej <niechwiej.mateusz@gmail.com>
2021-10-10 15:08:49 +0100

    Feature: Add sorting functionality

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product, Category, Colour
+from .models import Product, Category
 
 # Create your views here.
 
@@ -13,26 +13,46 @@ def products(request):
     products = Product.objects.all()
     query = None
     category = None
+    sort= None
+    direction = None
 
     if request.GET:
-        if 'category' in request.GET:
-            categories = request.GET['category'].split(',')
-            products = products.filter(category__name__in=categories)
-            categories = Category.objects.filter(name__in=categories)
+            if 'category' in request.GET:
+                categories = request.GET['category'].split(',')
+                products = products.filter(category__name__in=categories)
+                categories = Category.objects.filter(name__in=categories)
 
-        if 'q' in request.GET:
-            query = request.GET['q']
-            if not query:
-                messages.error(request, "Ups, you didn't enter any search criteria!")
-                return redirect(reverse('products'))
-            
-            queries = Q(name__icontains=query) | Q(description__icontains=query)
-            products = products.filter(queries)
+            if 'q' in request.GET:
+                query = request.GET['q']
+                if not query:
+                    messages.error(request, "Ups, you didn't enter any search criteria!")
+                    return redirect(reverse('products'))
+                
+                queries = Q(name__icontains=query) | Q(description__icontains=query)
+                products = products.filter(queries)
+
+            if 'sort' in request.GET:
+                sortkey = request.GET['sort']
+                sort = sortkey
+            if sortkey == 'category':
+                sortkey = 'category__name'
+            else:
+                if sortkey == 'name':
+                    sortkey = 'lower_name'
+                    products = products.annotate(lower_name=Lower('name'))
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
+    sorting = f'{sort}_{direction}'
 
     context = {
         'products': products,
         'search_phrase': query,
         'selected_category': category,
+        'sorting': sorting,
     }
 
     return render(request, 'products/products.html', context)
