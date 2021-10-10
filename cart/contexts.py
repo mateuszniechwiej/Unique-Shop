e7b7b41669adf8c2f897b8e3dd30cb1a95064933
mateuszniechwiej <niechwiej.mateusz@gmail.com>
2021-10-10 22:11:39 +0100

    Feature: Cart grand_total updating in navbar

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,6 +1,7 @@
 from django.conf import settings
-
+from django.shortcuts import get_object_or_404
 from unique_shop.settings import DELIVERY_FEE
+from products.models import Product
 
 def cart_contents(request):
 
@@ -8,6 +9,17 @@ def cart_contents(request):
     total = 0
     product_count = 0
     grand_total = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, item_qty in cart.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += item_qty * product.price
+        product_count += item_qty
+        cart_items.append({
+            'item_id': item_id,
+            'item_qty': item_qty,
+            'product': product,
+        })
 
     if total < settings.FREE_DELIVERY and total != 0:
         delivery = total + settings.DELIVERY_FEE
