7a84d4115b59e96bd5ca4a3434ba3095266666c3
mateuszniechwiej <niechwiej.mateusz@gmail.com>
2021-10-18 19:08:50 +0100

    Add stripe elements

--- checkout/forms.py
+++ checkout/forms.py
@@ -24,7 +24,7 @@ class OrderForm(forms.ModelForm):
             'town_or_city': 'Town or City',
             'street_address1': 'Street Address 1',
             'street_address2': 'Street Address 2',
-            'county': 'County ,State or Locality',
+            'county': 'County, State or Locality',
         }
 
         self.fields['full_name'].widget.attrs['autofocus'] = True
--- checkout/views.py
+++ checkout/views.py
@@ -12,7 +12,9 @@ def checkout(request):
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
-        'order_form': order_form,   
+        'order_form': order_form,
+        'stripe_public_key': 'pk_test_51JlxhHBdOsb29ESmtf5eDVHLFBDzDSKwN9F5rwzWWekzTnWpHuadIzD1QXHvVYReTAs4rxj0oL1TmPNZH3V2aBBL00OCvhGMCy', 
+        'client_secret': 'testing client secret',  
     }
 
     return render(request, template, context)
