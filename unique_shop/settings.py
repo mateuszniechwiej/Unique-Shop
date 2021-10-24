988d5555019756cc7aca8ce3abd3287110554b97
mateuszniechwiej <niechwiej.mateusz@gmail.com>
2021-10-24 23:29:09 +0100

    Set debug in settings

--- unique_shop/settings.py
+++ unique_shop/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['mn-unique-shop.herokuapp.com', 'localhost:8000', '127.0.0.1']
 
