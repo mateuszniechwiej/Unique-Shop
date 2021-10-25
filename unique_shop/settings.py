dcf696e1930e70a00a8adc386a128a3f7c05e506
mateuszniechwiej <niechwiej.mateusz@gmail.com>
2021-10-25 15:50:50 +0100

    Add cache control

--- unique_shop/settings.py
+++ unique_shop/settings.py
@@ -196,6 +196,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    #cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'mn-unique-shop'
     AWS_S3_REGION_NAME = 'eu-west-1'
