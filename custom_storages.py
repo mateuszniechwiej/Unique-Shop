715c0d8c855cd6a78f98f7a8acaf9fce27b4115f
mateuszniechwiej <niechwiej.mateusz@gmail.com>
2021-10-25 17:39:11 +0100

    Update Debug settings to True

--- custom_storages.py
+++ custom_storages.py
@@ -5,5 +5,6 @@ from storages.backends.s3boto3 import S3Boto3Storage
 class StaticStorage(S3Boto3Storage):
     location = settings.STATICFILES_LOCATION
 
+
 class MediaStorage(S3Boto3Storage):
     location = settings.MEDIAFILES_LOCATION
